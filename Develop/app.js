fe90ad92677f13aea20a97af9ad8714a3931e9e2
Rich Capuy <rccapuy@gmail.com>
2020-05-09 18:59:44 -0400

    Completed functionality

--- Develop/app.js
+++ Develop/app.js
@@ -4,32 +4,95 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+
+// New members
+const members = [];
+
+// Display initial prompts to New Member
+function addMember(){
+	inquirer.prompt([
+		{
+			type: "input",
+			name: "name",
+			message:"Enter new team member's name"
+		},
+		{
+			type: "list",
+			name: "role",
+			message:"Select new team member's role",
+			choices: [
+				"Manager",
+				"Engineer",
+				"Intern"
+			]
+		},
+		{
+			type: "input",
+			name: "id",
+			message:"Enter new team member's id",
+		},
+		{
+			type: "input",
+			name: "email",
+			message:"Enter new team member's email address",
+		},
+	])
+	.then(function newMember({name, role, id, email}){
+		// Role specific properties
+		let roleProperty;
+		if (role === "Manager"){
+			roleProperty = "office number";
+		} else if (role === "Intern"){
+			roleProperty = "school";
+		} else {
+			roleProperty = "github";
+		}
+		inquirer.prompt([
+			{
+				type: "input",
+				name: "roleProperty",
+				message: `Enter new team member's ${roleProperty} username`
+			},
+			{
+				type: "confirm",
+				name: "moreMembers",
+				message: "Would you like to add more team members?"
+			}
+		])
+		.then(function({roleProperty, moreMembers}){
+			let newMember;
+			if (role === "Manager"){
+				newMember = new Manager(name, id, email, roleProperty);
+			} else if (role === "Intern"){
+				newMember = new Intern(name, id, email, roleProperty);
+			} else {
+				newMember = new Engineer(name, id, email, roleProperty)
+			}
+			// Add newMember to list
+			members.push(newMember);
+			if (moreMembers == true){
+				addMember();
+			} else {
+				finishHTML();
+			}
+		})
+	})
+}
+// Function to write HTML file with members
+function finishHTML(){
+	const template = render(members);
+    fs.writeFile("./output/team.html", template, function (err) {
+        if (err) {
+            console.log(err);
+        };
+    });
+    console.log("Congrats Your Software Engineering Team Is Complete!");
+}
+
+// Run addMember function
+addMember();
--- Develop/index.js
+++ Develop/index.js
@@ -2,94 +2,97 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-// console.log(inquirer);
-// const path = require("path");
-// const fs = require("fs");
-// ​
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
-// ​
-// const render = require("./lib/htmlRenderer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+// New members
+const members = [];
 
 // Display initial prompts to New Member
-inquirer.prompt([
-	{
-		type: "input",
-		name: "name",
-		message:"Enter new team member's name"
-	},
-	{
-		type: "list",
-		name: "role",
-		message:"Select new team member's role",
-		choices: [
-			"Manager",
-			"Engineer",
-			"Intern"
-		]
-	},
-	{
-		type: "input",
-		name: "id",
-		message:"Enter new team member's id",
-	},
-	{
-		type: "input",
-		name: "email",
-		message:"Enter new team member's email address",
-	},
-])
-.then(function newMember({name, role, id, email}){
-	// Role specific properties
-	let roleProperty;
-	if (role === "Manager"){
-		roleProperty = "office number";
-		// inquirer.prompt(
-		// 	{
-		// 		type: "input",
-		// 		name: roleProperty,
-		// 		message:"Enter new team member's " + roleProperty,
-		// 	},
-		// )
-		// let newMember = new Manager(name, id, email, roleProperty)
-		// console.log(newMember.roleProperty)
-	} else if (role === "Intern"){
-		roleProperty = "school";
-		// inquirer.prompt(
-		// 	{
-		// 		type: "input",
-		// 		name: roleProperty,
-		// 		message:"Enter new team member's " + roleProperty,
-		// 	},
-		// )
-		// let newMember = new Intern(name, id, email, roleProperty)
-		// console.log(Intern(name, id, email, roleProperty));
-	} else {
-		roleProperty = "github";
-		// inquirer.prompt(
-		// 	{
-		// 		type: "input",
-		// 		name: roleProperty,
-		// 		message:"Enter new team member's link to " + roleProperty + " profile",
-		// 	},
-		// )
-		// let newMember = new Engineer(name, id, email, roleProperty)
-		// console.log(newMember.roleProperty)
-	}
+function addMember(){
 	inquirer.prompt([
 		{
 			type: "input",
-			name: "roleProperty",
-			message: `Enter new team member's ${roleProperty}`
+			name: "name",
+			message:"Enter new team member's name"
 		},
 		{
-			type: "confirm",
-			name: "moreMembers",
-			message: "Would you like to add more team members?"
-		}
+			type: "list",
+			name: "role",
+			message:"Select new team member's role",
+			choices: [
+				"Manager",
+				"Engineer",
+				"Intern"
+			]
+		},
+		{
+			type: "input",
+			name: "id",
+			message:"Enter new team member's id",
+		},
+		{
+			type: "input",
+			name: "email",
+			message:"Enter new team member's email address",
+		},
 	])
-	.then(function(){
-		console.log(role);
-		console.log(roleProperty);
+	.then(function newMember({name, role, id, email}){
+		// Role specific properties
+		let roleProperty;
+		if (role === "Manager"){
+			roleProperty = "office number";
+		} else if (role === "Intern"){
+			roleProperty = "school";
+		} else {
+			roleProperty = "github";
+		}
+		inquirer.prompt([
+			{
+				type: "input",
+				name: "roleProperty",
+				message: `Enter new team member's ${roleProperty} username`
+			},
+			{
+				type: "confirm",
+				name: "moreMembers",
+				message: "Would you like to add more team members?"
+			}
+		])
+		.then(function({roleProperty, moreMembers}){
+			let newMember;
+			if (role === "Manager"){
+				newMember = new Manager(name, id, email, roleProperty);
+			} else if (role === "Intern"){
+				newMember = new Intern(name, id, email, roleProperty);
+			} else {
+				newMember = new Engineer(name, id, email, roleProperty)
+			}
+			// Add newMember to list
+			members.push(newMember);
+			if (moreMembers == true){
+				addMember();
+			} else {
+				finishHTML();
+			}
+		})
 	})
-})
+}
+// Function to write HTML file with members
+function finishHTML(){
+	const template = render(members);
+    fs.writeFile("./output/team.html", template, function (err) {
+        if (err) {
+            console.log(err);
+        };
+    });
+    console.log("Congrats Your Software Engineering Team Is Complete!");
+}
+
+// Run addMember function
+addMember();
